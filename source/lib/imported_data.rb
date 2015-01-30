93b2968552f1ec361d368c2236df8314051d10ec
mevanoff24 & rgrewalucsb <pair+mevanoff24.rgrewalucsb@devbootcamp.com>
2015-01-30 13:56:49 -0800

    editied the seed locations

--- source/lib/imported_data.rb
+++ source/lib/imported_data.rb
@@ -1,13 +1,17 @@
 require_relative "parser"
+require 'pathname'
 
-CSVParser.set_source('businesses.csv')
+APP_ROOT = Pathname.new(File.dirname(File.expand_path(__FILE__))).parent
+filename =APP_ROOT.join('db', 'data')
+
+CSVParser.set_source(filename + 'businesses.csv')
 restaurant_data = CSVParser.parse_to_hashes
 p restaurant_data[0]
 
-CSVParser.set_source('inspections.csv')
+CSVParser.set_source(filename + 'inspections.csv')
 inspection_data = CSVParser.parse_to_hashes
 p inspection_data[0]
 
-CSVParser.set_source('violations.csv')
+CSVParser.set_source(filename + 'violations.csv')
 violation_data = CSVParser.parse_to_hashes
 p violation_data[0]
