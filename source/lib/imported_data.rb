8585e63c1cc2d2220a73d6db1ce29087e5c274a6
Randy Grewal <rgrewalucsb@gmail.com>
2015-01-30 13:08:30 -0800

    Completed parsing the data for all three files and they are in the form of seperate arrays of hashes, with corresponding columns as symbols

--- source/lib/imported_data.rb
+++ source/lib/imported_data.rb
@@ -1,13 +1,13 @@
 require_relative "parser"
 
-CSVParser.set_source('businesses.csv')
-restaurant_data = CSVParser.parse_to_hashes
-p restaurant_data
+# CSVParser.set_source('businesses.csv')
+# restaurant_data = CSVParser.parse_to_hashes
+# p restaurant_data
 
-CSVParser.set_source('inspections.csv')
-inspection_data = CSVParser.parse_to_hashes
-p inspection_data
+# CSVParser.set_source('inspections.csv')
+# inspection_data = CSVParser.parse_to_hashes
+# p inspection_data
 
 CSVParser.set_source('violations.csv')
 violation_data = CSVParser.parse_to_hashes
-p violation_data
+p violation_data[0]
